3003691c7a24c539486c81b770b3869b9cadcb91
johnchem <j.payssan@gmail.com>
2023-05-15 22:02:57 +0200

    comments : modification of the linked object display

--- softdesk/comments/serializers.py
+++ softdesk/comments/serializers.py
@@ -2,10 +2,33 @@ from rest_framework import serializers
 
 from comments.models import Comments
 
-class CommentsSerializer(serializers.ModelSerializer):
+from issues.serializers import IssuesSerializer
 
+
+class CommentsListSerializer(serializers.ModelSerializer):
     class Meta:
         model=Comments
-        fields="__all__"
+        fields=(
+            "comment_id",
+            "author_user_id",
+            "description",
+            "created_time",
+        )
+
+    def __str__(self):
+        return f"#{self.comment_id} - {self.author_user_id} : {self.description}"
+
+class CommentsDetailSerializer(serializers.ModelSerializer):
+    issues_id = IssuesSerializer()
+    class Meta:
+        model=Comments
+        fields=(
+            "comment_id",
+            "description",
+            "author_user_id",
+            "issues_id",
+            "created_time",
+        )
+
 
     
--- softdesk/comments/views.py
+++ softdesk/comments/views.py
@@ -5,24 +5,28 @@ from issues.models import Issues
 from authentification.models import User
 from comments.models import Comments
 
-from comments.serializers import CommentsSerializer
+from comments.serializers import CommentsDetailSerializer, CommentsListSerializer
 
 # Create your views here.
 
 class CommentsViewset(viewsets.ModelViewSet):
-    serializer_class=CommentsSerializer
+    serializer_class=CommentsDetailSerializer
+    list_serializer_class=CommentsListSerializer
 
     def get_queryset(self):
         return Comments.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return self.list_serializer_class
+        return super().get_serializer_class()
     
     def create(self, request, project_pk=None, issue_pk=None):
         data = request.data.copy()
         data['issues_id']=Issues.objects.get(pk=issue_pk).issues_id
         data['author_user_id']=request.user.user_id
 
-        serializer=CommentsSerializer(
-            data=data
-        )
+        serializer=self.serializer_class(data=data)
 
         if not serializer.is_valid():
              return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -36,7 +40,7 @@ class CommentsViewset(viewsets.ModelViewSet):
         except comment.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = CommentsSerializer(comment, data=request.data, partial=True)
+        serializer = self.serializer_class(comment, data=request.data, partial=True)
         # setting raise_exception=True in the serializer's is_valid method will raise exception on error. So you don't have implement extra logics
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
